c58c9c8b4f2c0387de363a903407e96232da78e3
Mario <mario.ibarrag@hotmail.com>
2022-02-20 20:08:40 -0600

    desafio 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log("main.js file loaded");
+
+function calcularPromedio(){
+    let stringNumeros = prompt('Ingresar números separados por "," (coma)');
+    
+    if(stringNumeros != null){
+
+        if(validateNumbersOnly(stringNumeros)){
+            let array = stringNumeros.split(',');
+            let suma = 0;
+            for(let i = 0; i < array.length; i++){
+                suma += parseInt(array[i].trim());
+            }
+            alert("El promedio es: " + (suma/array.length));
+        } else{
+            alert("Solo se permiten números separados por coma.")
+        }
+        
+    }
+
+}
+
+function validateNumbersOnly(string){
+    for(let i = 0; i < string.length; i++){
+        if(isNaN(string[i]) ){
+            if(string[i] != ',')
+                return false;
+        }
+    }
+    return true;
+}
